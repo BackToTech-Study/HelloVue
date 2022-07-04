0450f5ec6a72afc8622720c234eda44f1735d476
Dorian1995 <92944289+Dorian1995@users.noreply.github.com>
2022-07-04 20:18:54 +0300

    Added Slots

---
+++ Slots.vue
@@ -0,0 +1,18 @@
+<script>
+import ChildComp from './CComp.vue'
+
+export default {
+  components: {
+    ChildComp
+  },
+  data() {
+    return {
+      msg: 'from parent'
+    }
+  }
+}
+</script>
+
+<template>
+  <ChildComp>Message: {{ msg }}</ChildComp>
+</template>
